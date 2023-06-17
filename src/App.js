4487fbf8fdcbbc64ca10d64ec6bd7651f15c4b04
Deepak darak <76906091+Deepak-Darak@users.noreply.github.com>
2023-06-17 16:36:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Payment from "./components/Payment";
+import "./style.scss";
 
-function App() {
+const App = () => {
+  const [state, setState] = useState("start");
+
+  const handleClick = () => {
+    setState("pay");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <Navbar />
+
+      {state == "start" && (
+        <div className="container">
+          <h1>
+            "Food nourishes the body
+            <br />
+            Sharing nourishes the soul"
+          </h1>
+          <p>
+            Close to a billion people - one-eighth of the world’s population
+            still live in hunger. Each year 2 million children die through
+            malnutrition.
+            <br />
+            This is happening at a time when doctors in Britain are warning of
+            the spread of obesity. We are eating too much while others starve.
+          </p>
+          <button onClick={handleClick}>Lets's Feed❤️</button>
+        </div>
+      )}
+      {state == "pay" && <Payment />}
     </div>
   );
-}
+};
 
 export default App;
